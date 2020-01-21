79f1fce2775edaabd3ce7d10e20e30e64378e747
DELTA\BARRY.KE <barry.ke@deltaww.com>
2020-01-21 10:33:30 +0800

    txt to csv

--- tools/simnet/preprocess/MRPC/Data_Transfer.py
+++ tools/simnet/preprocess/MRPC/Data_Transfer.py
@@ -5,33 +5,47 @@ version: September 23, 2019 11:39 AM
 Last revision: January 21, 2020 09:17 AM
   
 Author : Chao-Hsuan Ke
-''' 
+'''
 
 import csv
 import codecs
 
 # read csv
 csvfile = 'train_utf8.csv'
-#file = open(csvfile,'r',encoding="utf-8")
+
+# read txt
+filePath = "C:\\Users\\barry.ke\\Desktop\\MSRParaphraseCorpus\\";  # file path
+fileName = "msr_paraphrase_train.txt";  # file name
+ftxt = open(filePath + fileName, 'r', encoding="utf-8")
 
 # output csv
-outputcsvfile = 'output.csv'
+outputcsvfile = 'C:\\Users\\barry.ke\\Desktop\\output.csv'
 f = open(outputcsvfile, "w", encoding="utf-8", newline='')
 w = csv.writer(f)
-#w.writerow(['id', 'qid1', 'qid2', 'question1', 'question2', 'is_duplicate'])
 quertId = -1
-questionId = 0
+# questionId = 0
 
-with codecs.open(csvfile, "r", encoding='utf-8') as csv:
-    for line in csv:
-        if line != "":
-            cols = line.split(",")
-            if(len(cols) > 5):
-                questionId2 = questionId+1
-                #print(quertId, cols[3])
-                w.writerow([quertId, questionId, questionId2, cols[3], cols[4], cols[5]])
-                quertId+=1
-                questionId = questionId2
-                questionId+=1
+# csv to csv
+# with codecs.open(csvfile, "r", encoding='utf-8') as csv:
+#     for line in csv:
+#         if line != "":
+#             cols = line.split(",")
+#             if(len(cols) > 5):
+#                 questionId2 = questionId+1
+#                 #print(quertId, cols[3])
+#                 w.writerow([quertId, questionId, questionId2, cols[3], cols[4], cols[5]])
+#                 quertId+=1
+#                 questionId = questionId2
+#                 questionId+=1
+#
+# csv.close()
 
-csv.close()
+
+# txt to csv
+for line in ftxt:
+    cols = line.split("	")
+    if (len(cols) == 5):
+        w.writerow([quertId, cols[1], cols[2], cols[3], cols[4], cols[0]])
+        quertId += 1
+
+ftxt.close()
